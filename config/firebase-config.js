bb2ec391b6c853cf423c16b5c3de710ce3678658
roiguri <155663297+roiguri@users.noreply.github.com>
2024-08-20 20:38:55 +0300

    Add storage to config

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-// Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
