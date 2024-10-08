d961a4a0c966426c0c1e50e165c16316dbe1edb5
roiguri <155663297+roiguri@users.noreply.github.com>
2024-10-08 22:24:31 +0300

    Add service-worker functionality

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,17 @@
-const CACHE_NAME = 'our-kitchen-chronicles-v1';
+const CACHE_NAME = 'our-kitchen-chronicles-2024.10.08,22:23';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/pages/recipe-page.html',
-  '/styles/main.css',
-  '/js/navigation-script.js',
-  '/js/recipe-data.js',
-  '/js/featured-recipes.js',
+  '/styles/',
   '/img/category-jars/optimized/',
   '/img/background/stone-counter-top.jpg',
-  '/img/background/wood-texture.jpg'
+  '/img/background/wood-texture.jpg',
+  '/styles/main.css',
+  '/styles/base.css',
+  '/styles/layout.css',
+  '/styles/components.css',
+  '/styles/pages.css',
+  '/styles/responsive.css',
   // Add other critical assets here
 ];
 
@@ -23,15 +25,58 @@ self.addEventListener('install', function(event) {
   );
 });
 
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
+        // Cache hit - return response
         if (response) {
           return response;
         }
-        return fetch(event.request);
-      }
-    )
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            // Check if the request is for an image in the optimized folder
+            if (event.request.url.includes('/img/category-jars/optimized/')) {
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+            }
+
+            return response;
+          }
+        );
+      })
   );
 });
