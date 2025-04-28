fe79b867e24f947379acaad25938e4eb95d58c91
Roi Guri <roiguri@gmail.com>
2025-04-28 23:17:47 +0300

    Migrate documents page to vite structure

---
+++ src/pages/vite-documents.js
@@ -0,0 +1,21 @@
+// For all pages
+import '../js/sw-register.js';
+import '../js/config/firebase-config.js'
+
+// Page specific:
+import '../js/navigation-script.js';
+import '../js/documents.js';
+
+// General Components
+import '../lib/utilities/modal/modal.js';
+import '../lib/search/header-search-bar/header-search-bar.js';
+import '../lib/auth/auth-controller.js';
+import '../lib/auth/components/auth-content.js';
+import '../lib/auth/components/auth-avatar.js';
+import '../lib/auth/components/login-form.js';
+import '../lib/auth/components/signup-form.js';
+import '../lib/auth/components/forgot-password.js';
+import '../lib/auth/components/user-profile.js';
+
+// Specific page components
+import '../lib/utilities/pdf_viewer/pdf_viewer.js'
