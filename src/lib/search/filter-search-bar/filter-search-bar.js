0e0262967a18512192a68d15c35dc2180923642f
Roi Guri <roiguri@gmail.com>
2026-04-23 18:14:51 +0300

    fix: increase search bar padding to avoid clear button overlap

--- src/lib/search/filter-search-bar/filter-search-bar.js
+++ src/lib/search/filter-search-bar/filter-search-bar.js
@@ -54,7 +54,7 @@ class FilterSearchBar extends HTMLElement {
         .search-input {
           width: 100%;
           height: 38px;
-          padding: 0 18px 0 44px;
+          padding: 0 44px 0 44px; /* Balanced padding for icon and clear button */
           border: 0;
           background: transparent;
           outline: none;
@@ -117,7 +117,7 @@ class FilterSearchBar extends HTMLElement {
 
         :host([dir="rtl"]) .search-input {
           padding-right: 44px;
-          padding-left: 18px;
+          padding-left: 44px;
         }
 
         :host([dir="rtl"]) .search-icon {
